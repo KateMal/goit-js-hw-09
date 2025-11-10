dcf6987c6ab0e8e069b147c597caa816c6d4e9c4
KateMal <katemalushko@gmail.com>
2025-11-10 18:49:54 +0100

    2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,30 @@
-console.log('JS is connected');
+const formData = {
+  email: '',
+  message: '',
+};
+const elForm = document.querySelector('.feedback-form');
+elForm.addEventListener('input', onFormInput);
+elForm.addEventListener('submit', onFormSubmit);
+function onFormInput(evt) {
+  // console.log(evt.target.name);
+  // console.log(evt.target.value);
+  formData[evt.target.name] = evt.target.value.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(formData);
+  evt.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+populateForm();
+function populateForm() {
+  const savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    formData.email = parsedData.email || '';
+    formData.message = parsedData.message || '';
+    elForm.email.value = formData.email;
+    elForm.message.value = formData.message;
+  }
+}
